276e6405c4324a825b131e1f9be757f45f8f8c7c
choisungwoo <sqlstyle@naver.com>
2022-07-24 21:52:12 +0900

    handleClickCategory Event

--- client/src/components/header/MarketCategory.tsx
+++ client/src/components/header/MarketCategory.tsx
@@ -3,24 +3,47 @@ import './Header.css'
 import Search from './Search'
 import React from 'react';
 import {useState} from 'react'
+import { tab } from '@testing-library/user-event/dist/tab';
 
 //Header 컴포넌트 메게변수 타입을 직접 선언합니다.
-interface categoryProps {
-    viewType : Number
+interface MarketCategoryType {
+    viewType : Number,
+    name : String
 }
 
 function MarketCategory() {
-    // 카테고리 클릭 이벤트
-    const handlerClickCategory = () => {
-        alert("dfdf")
-    }
+    const [exchangeView, setExchangeView] = useState(true);
+    const [issueView, setIssueView] = useState(false);
+    const [premiumView, setPremiumView] = useState(false);
+
+    const handleClickCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    
+        const target: any = event.currentTarget.classList.value;
+
+        console.log("target", target)
+        //setClickedButton(button.name);
+        switch(target) {
+            case "exchange":
+                    setExchangeView(true)
+                break;
+            case "issue":
+                !issueView ? setIssueView(true) : null
+                break;
+        }
+      };
 
     const category = ["시세", "이슈", "김프", "트렌드", "선물"]
+    const categoryKey = ["exchange", "issue", "premium", "trend", "ddd"]
+    let tabCount = 0;
+
     const categoryList = category.map((menu) => {
         if(menu === "시세") {
-            return(<span onClick={handlerClickCategory}><strong className = "select" >{menu}</strong></span>)
+            tabCount ++;
+            return(<span onClick={handleClickCategory} className={categoryKey[tabCount]}><strong className = "select" >{menu}</strong></span>)
         }
-        return(<span><strong>{menu}</strong></span>)
+        tabCount ++;
+        return(<span onClick={handleClickCategory} className={categoryKey[tabCount]}><strong>{menu}</strong></span>)
     });
 
   return (
