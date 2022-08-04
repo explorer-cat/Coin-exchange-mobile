3769ca631813b6fe932587e94b8ed3833f708ea3
choisungwoo <sqlstyle@naver.com>
2022-08-04 22:58:29 +0900

    serach css modify

--- client/src/components/exchange/Search.tsx
+++ client/src/components/exchange/Search.tsx
@@ -10,10 +10,10 @@ function Search({loading}:SearchType) {
   if(loading) {
     return (
       <span className = "navBar_search-box">
-        <div className = "search-icon"></div>
-        <input className = "search-input-box" placeholder='코인명/심볼검색'>
-
-        </input>
+        <div className = "publicInput">
+        <Skeleton sx={{bgcolor: "rgba(255, 255, 255, 0.13)",width : "250px",height : "60px"}}/>
+        {/* <input className = "search-input-box" placeholder='코인명/심볼검색'/> */}
+        </div>
       </span>
   );
   } else {
