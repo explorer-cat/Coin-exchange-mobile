c24c62d3be14b10a4fd1b3265ad7454d0877f43f
choisungwoo <sqlstyle@naver.com>
2022-09-04 23:41:42 +0900

    content area sample 제작

---
+++ client/src/components/exchange/cryptoDetail/content/InvestmentInfo.tsx
@@ -0,0 +1,21 @@
+import '../../../../stylesheets/initialization.css'
+import '../../../../stylesheets/palette.css'
+import './DetailContent.css'
+import CategoryToggle from './ContentCategoryToggle'
+import React, {useEffect, useState, useCallback} from 'react';
+
+
+function ExchangeContent(): React.ReactElement {
+    const [loading, setLoading] = useState(false)
+
+
+    return (<>
+            하이용
+          </>
+
+    );
+}
+
+
+export default ExchangeContent;
+
